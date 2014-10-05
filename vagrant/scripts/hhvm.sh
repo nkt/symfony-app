6ded28453c826b4d5ba9724065d38534a1c001b0
Nikita Gusakov <dev@nkt.me>
2014-10-05 20:26:32 +0400

    Fixed hhvm installation

--- vagrant/scripts/hhvm.sh
+++ vagrant/scripts/hhvm.sh
@@ -3,7 +3,7 @@
 echo "Installing hhvm..."
 
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
-sudo add-apt-repository -y "deb http://dl.hhvm.com/ubuntu precise main"
+echo deb http://dl.hhvm.com/ubuntu trusty main | sudo tee /etc/apt/sources.list.d/hhvm.list
 
 sudo apt-get update
 
